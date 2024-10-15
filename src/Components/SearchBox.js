6080175cfd4ae7bf1e88c2a452a628a600f8d6c9
utsab roy <roy.utsab01@gmail.com>
2024-10-15 14:44:04 +0530

    work__151024

--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -154,7 +154,8 @@ useEffect(()=>{
             name="select_district"
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
-            value={district_def_Valu ? district_def_Valu : formik.values.select_district}
+            // value={district_def_Valu ? district_def_Valu : formik.values.select_district}
+            value={formik.values.select_district}
           >
             <option value='0'>Select District *</option>
             {getDistrictList?.map((option) => ( 
@@ -177,7 +178,7 @@ useEffect(()=>{
           name="select_range"
           onChange={formik.handleChange}
           onBlur={formik.handleBlur}
-          value= {range_def_Valu ? range_def_Valu : formik.values.select_range}
+          value= {formik.values.select_range}
           >
 
           {getRangeList.length < 1 ? (
@@ -212,7 +213,7 @@ useEffect(()=>{
             name="select_type"
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
-            value= {type_def_Valu ? type_def_Valu : formik.values.select_type}
+            value= {formik.values.select_type}
           >
             <option value='0'>Select Society Type</option>
             {getSocietyType.map((option) => (
@@ -235,7 +236,7 @@ useEffect(()=>{
 			      placeholder="Society Name"
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
-            value= {soci_Name_def_Valu ? soci_Name_def_Valu : formik.values.society_Name}
+            value= {formik.values.society_Name}
           />
           {formik.errors.society_Name && formik.touched.society_Name && (
             <div className="required">{formik.errors.society_Name}</div>
