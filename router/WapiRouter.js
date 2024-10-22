0174e0e741b30ded5fe514eadb519023d7ab0059
meettan <mondal.tanmoy@synergicsoftek.com>
2024-10-22 13:50:49 +0530

    chnage

--- router/WapiRouter.js
+++ router/WapiRouter.js
@@ -259,6 +259,17 @@ var moment = require('moment');
 
   WapiRouter.post('/getsocdetail_list', async(req, res) => {
     var formdata = req.body;
+    var dist = formdata.dist_id > 0 ? `AND a.dist_code=${formdata.dist_id} ` : '';
+    var range = formdata.range_code > 0 ? `AND a.range_code=${formdata.range_code} ` : '';
+    var soc_type = formdata.soc_type_id > 0 ? `AND a.soc_type=${formdata.soc_type_id} ` : '';
+    if (formdata.socname && formdata.socname.trim() !== '') {
+      var socname = `AND a.cop_soc_name LIKE '%${formdata.socname.split("'").join("\\'")}%' `;
+    }else{
+      var socname = '';
+    }
+    //soc_data_status = `AND a.approve_status='A' `;
+   
+   
     var select = "a.cop_soc_name,a.reg_no,a.reg_date,b.soc_type_name,f.soc_tier_name,h.controlling_authority_type_name as reg_cont_auth,g.controlling_authority_name as returning_officer,st.state_name,c.dist_name,d.zone_name,e.range_name,a.urban_rural_flag,ulcat.ulb_catg_name,ulb.ulb_name,wa.ward_name,mb.block_name,gp.gp_name,vill.vill_name,a.pin_no,a.address,mms.manage_status_name,mot.officer_type_name,a.num_of_memb,a.audit_upto,a.last_elec_date,a.tenure_ends_on,a.contact_name as key_person,a.contact_designation as key_person_desig,a.contact_number,a.email,a.case_id,a.case_num,a.functional_status",
     table_name = `md_society a LEFT JOIN md_society_type b ON a.soc_type = b.soc_type_id LEFT JOIN md_district c ON a.dist_code = c.dist_code 
     LEFT JOIN md_controlling_authority_type h ON a.cntr_auth_type = h.controlling_authority_type_id 
@@ -273,8 +284,10 @@ var moment = require('moment');
     LEFT JOIN md_management_status mms ON a.mgmt_status = mms.manage_status_id
     LEFT JOIN md_officer_type mot ON a.officer_type = mot.officer_type_id
     LEFT JOIN md_zone d ON a.zone_code = d.zone_id LEFT JOIN md_range e ON a.range_code = e.range_id LEFT JOIN md_soc_tier f ON a.soc_tier = f.soc_tier_id `,
-    where = null,
+   
     order = null;
+    var maincon = dist+range+soc_type+socname;
+    var where = `1 ${maincon}`;
     var res_dt = await db_Select(select, table_name, where, order);
   //  var where1 = `soc_id = '${formdata.soc_id}'`;
     //var bord_member_detail = await db_Select('board_memb_name,board_memb_desig,bm_contact_no', `td_board_member`, where1, order);
