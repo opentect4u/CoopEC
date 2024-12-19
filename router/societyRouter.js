737350731df14314b30698ef8596ee2e19ce5219
meettan <mondal.tanmoy@synergicsoftek.com>
2024-12-19 15:34:18 +0530

    chnage

--- router/societyRouter.js
+++ router/societyRouter.js
@@ -53,6 +53,8 @@ SocietyRouter.get("/edit", async (req, res) => {
     // Extract range_id from session
     const soc_id = req.query.id;
     var range_id = req.session.user.range_id;
+    var cntr_auth_type = req.session.user.cntr_auth_type;
+    console.log(cntr_auth_type);
     const select = "*";
     const table_name = "md_society";
     const whr = `id='${soc_id}' `;
@@ -811,19 +813,32 @@ SocietyRouter.get("/modifiedlist", async (req, res) => {
   try {
     // Extract range_id from session
     const range_id = req.session.user.range_id;
-    const select =
-      "a.id,a.cop_soc_name,a.reg_no,a.functional_status,b.soc_type_name,c.dist_name,d.zone_name,e.range_name,f.soc_tier_name";
-    if (range_id > 0) {
-      var table_name = `md_society a LEFT JOIN md_society_type b ON a.soc_type = b.soc_type_id LEFT JOIN md_district c ON a.dist_code = c.dist_code LEFT JOIN md_zone d ON a.zone_code = d.zone_id LEFT JOIN md_range e ON a.range_code = e.range_id LEFT JOIN md_soc_tier f ON a.soc_tier = f.soc_tier_id WHERE a.functional_status='Functional' AND a.range_code = "${range_id}" AND approve_status ='E' `;
-    } else {
-      var table_name = `md_society a LEFT JOIN md_society_type b ON a.soc_type = b.soc_type_id LEFT JOIN md_district c ON a.dist_code = c.dist_code LEFT JOIN md_zone d ON a.zone_code = d.zone_id LEFT JOIN md_range e ON a.range_code = e.range_id LEFT JOIN md_soc_tier f ON a.soc_tier = f.soc_tier_id WHERE a.functional_status='Functional' AND approve_status ='E'`;
-    }
-    whr = "";
-    const order = null;
-    if (range_id > 0) {
-      whr1 = `functional_status='Functional' AND range_code='${range_id}' AND approve_status ='E'`;
-    } else {
-      whr1 = `functional_status='Functional' AND approve_status ='E' `;
+    const cntr_auth_type = req.session.user.cntr_auth_type;
+    console.log(range_id,cntr_auth_type);
+    if(cntr_auth_type == 1){
+          var select =
+          "a.id,a.cop_soc_name,a.reg_no,a.functional_status,b.soc_type_name,c.dist_name,d.zone_name,e.range_name,f.soc_tier_name";
+        if (range_id > 0) {
+          var table_name = `md_society a LEFT JOIN md_society_type b ON a.soc_type = b.soc_type_id LEFT JOIN md_district c ON a.dist_code = c.dist_code LEFT JOIN md_zone d ON a.zone_code = d.zone_id LEFT JOIN md_range e ON a.range_code = e.range_id LEFT JOIN md_soc_tier f ON a.soc_tier = f.soc_tier_id WHERE a.functional_status='Functional' AND a.range_code = "${range_id}" AND approve_status ='E' `;
+        } else {
+          var table_name = `md_society a LEFT JOIN md_society_type b ON a.soc_type = b.soc_type_id LEFT JOIN md_district c ON a.dist_code = c.dist_code LEFT JOIN md_zone d ON a.zone_code = d.zone_id LEFT JOIN md_range e ON a.range_code = e.range_id LEFT JOIN md_soc_tier f ON a.soc_tier = f.soc_tier_id WHERE a.functional_status='Functional' AND approve_status ='E'`;
+        }
+        whr = "";
+        var order = null;
+        if (range_id > 0) {
+          whr1 = `functional_status='Functional' AND range_code='${range_id}' AND approve_status ='E'`;
+        } else {
+          whr1 = `functional_status='Functional' AND approve_status ='E' `;
+        }
+    }else{
+
+          var select =
+          "a.id,a.cop_soc_name,a.reg_no,a.functional_status,b.soc_type_name,c.dist_name,d.zone_name,e.range_name,f.soc_tier_name";
+          var table_name = `md_society a LEFT JOIN md_society_type b ON a.soc_type = b.soc_type_id LEFT JOIN md_district c ON a.dist_code = c.dist_code LEFT JOIN md_zone d ON a.zone_code = d.zone_id LEFT JOIN md_range e ON a.range_code = e.range_id LEFT JOIN md_soc_tier f ON a.soc_tier = f.soc_tier_id WHERE a.functional_status='Functional' AND a.dist_code = "${range_id}" AND approve_status ='E' `;
+        
+        var whr = "";
+        var order = null;
+        var whr1 = `functional_status='Functional' AND dist_code='${range_id}' AND approve_status ='E'`;
     }
 
     // Execute database query
